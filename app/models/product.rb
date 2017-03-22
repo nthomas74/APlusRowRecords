1995d04391e12758768a5b6e0dfa3e252b9e3dee
Nicole Thomas <nicolemichellethomas@gmail.com>
2017-03-22 20:55:51 +0000

    commit image uploads again?

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,4 @@ class Product < ApplicationRecord
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
-
 end
