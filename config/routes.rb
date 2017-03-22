5a689c288ed1593df6116f655e81a7c1ffd97e51
Nicole Thomas <nicolemichellethomas@gmail.com>
2017-03-22 21:32:11 +0000

    added products controller properly though this time

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources 'products', only: [:index, :show]
+
+  root to: 'products#index'
 end
