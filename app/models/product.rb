32f24d8c4e99ac0673cf983bc87f059ee95c9485
Nicole Thomas <nicolemichellethomas@gmail.com>
2017-03-22 20:51:16 +0000

    added image uploads

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :artist
   belongs_to :product_type
   has_many :orders
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170320162554) do
+ActiveRecord::Schema.define(version: 20170322203431) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,10 +84,15 @@ ActiveRecord::Schema.define(version: 20170320162554) do
     t.string   "description"
     t.decimal  "price"
     t.integer  "stock_quantity"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "artist_id"
     t.integer  "product_type_id"
+    t.string   "image"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "provinces", force: :cascade do |t|
