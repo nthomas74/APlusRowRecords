71821b926507606ae0c6c608109583b09d94f8a1
Nicole Thomas <nicolemichellethomas@gmail.com>
2017-04-01 16:22:04 +0000

    added pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,5 @@ class Product < ApplicationRecord
   has_attached_file :image, :styles => { :small => "160x160>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
+  paginates_per 5
 end
