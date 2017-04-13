05254ca1d45d6c662dcfaa91ab0f3ee8e12836e9
Nicole Thomas <nicolemichellethomas@gmail.com>
2017-04-13 01:24:04 +0000

    fixed rubocop offenses in products_controller except filter_data method because howwwww

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,33 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    if params.has_key?("srch_term")
-      if params[:product][:artist_id] != ''
-#@products = Product.joins(:artist).where('artists.name LIKE ? OR products.name LIKE ?', params[:srch_term], params[:srch_term]).page(params[:page]).per(5).order(:name)
-        @products = Product.joins(:artist, :product_type)
-          .where("artists.id = ? AND (artists.name ILIKE ? OR artists.name ILIKE ? OR artists.name ILIKE ? OR artists.name ILIKE ? OR products.name ILIKE ? OR products.name ILIKE ? OR products.name ILIKE ? OR products.name ILIKE ? OR product_types.name ILIKE ? OR product_types.name ILIKE ? OR product_types.name ILIKE ? OR product_types.name ILIKE ?)", \
-          "#{params[:product][:artist_id]}", \
-          "% #{params[:srch_term]}", "#{params[:srch_term]} %", "% #{params[:srch_term]} %", params[:srch_term], \
-          "% #{params[:srch_term]}", "#{params[:srch_term]} %", "% #{params[:srch_term]} %", params[:srch_term], \
-          "% #{params[:srch_term]}", "#{params[:srch_term]} %", "% #{params[:srch_term]} %", params[:srch_term] \
-          ).page(params[:page]).per(5).order(:name)
-      else
-        @products = Product.joins(:artist, :product_type)
-          .where("artists.name ILIKE ? OR artists.name ILIKE ? OR artists.name ILIKE ? OR artists.name ILIKE ? OR products.name ILIKE ? OR products.name ILIKE ? OR products.name ILIKE ? OR products.name ILIKE ? OR product_types.name ILIKE ? OR product_types.name ILIKE ? OR product_types.name ILIKE ? OR product_types.name ILIKE ?", \
-          "% #{params[:srch_term]}", "#{params[:srch_term]} %", "% #{params[:srch_term]} %", params[:srch_term], \
-          "% #{params[:srch_term]}", "#{params[:srch_term]} %", "% #{params[:srch_term]} %", params[:srch_term], \
-          "% #{params[:srch_term]}", "#{params[:srch_term]} %", "% #{params[:srch_term]} %", params[:srch_term] \
-          ).page(params[:page]).per(5).order(:name)
-      end
-  #  @products = Product.joins(:artist, :product_type).where(:artists.name @@ to_tsquery(params[:srch_term]))
-   else
-    #  @products = Product.joins(:artist, :product_type).where(:artists.name @@ to_tsquery(params[:srch_term]))
-     #@products = PgSearch.multisearch(params[:srch_term]).page(params[:page]).per(5).order(:name)
-     @products = Product.all.page(params[:page]).per(5).order(:name)
+    if params.key?('srch_term')
+      product_for_search
+    else
+      @products = Product.all.page(params[:page]).per(5).order(:name)
     end
-    # @search = Product.search  do
-    #   keywords(params[:srch_term])
-    # end
-    # @products = Product.page(params[:page]).per(5).order(:name)
     @artists = Artist.all.order(:name)
     @product_types = ProductType.all.order:name
   end
@@ -39,22 +16,78 @@ class ProductsController < ApplicationController
   def filter_data
     @artist = params[:product][:artist_id]
     @product_type = params[:product][:product_type_id]
-    @on_sale = params.has_key?("on_sale") ? "yes" : "no"
-    @new = params.has_key?("new") ? "yes" : "no"
+    @on_sale = params.key?('on_sale') ? 'yes' : 'no'
+    @new = params.key?('new') ? 'yes' : 'no'
 
-    @products = Product.joins(:artist, :product_type).where('artists.id = ?', @artist).where('product_types.id = ?', @product_type).where("products.on_sale = 'yes' OR products.on_sale = ?", @on_sale).where("products.new = 'yes' OR products.new = ?", @new).page(params[:page]).per(5) unless @artist == "" || @product_type == ""
-      if @artist == "" && @product_type == ""
-        @products = Product.where("products.on_sale = 'yes' OR products.on_sale = ?", @on_sale).where("products.new = 'yes' OR products.new = ?", @new).page(params[:page]).per(5)
+    unless @artist == '' || @product_type == ''
+      @products = Product.joins(:artist, :product_type)
+                         .where('artists.id = ?', @artist)
+                         .where('product_types.id = ?', @product_type)
+                         .where('products.on_sale = \'yes\' OR products.on_sale = ?', @on_sale)
+                         .where('products.new = \'yes\' OR products.new = ?', @new)
+                         .page(params[:page]).per(5)
+    end
+    if @artist == '' && @product_type == ''
+      @products = Product.where('products.on_sale = \'yes\' OR products.on_sale = ?', @on_sale)
+                         .where('products.new = \'yes\' OR products.new = ?', @new)
+                         .page(params[:page]).per(5)
 
-      elsif @artist == ""
-        @products = Product.joins(:product_type).where('product_types.id = ?', @product_type).where("products.on_sale = 'yes' OR products.on_sale = ?", @on_sale).where("products.new = 'yes' OR products.new = ?", @new).page(params[:page]).per(5)
-      elsif @product_type == ""
-        @products = Product.joins(:artist).where('artists.id = ?', @artist).where("products.on_sale = 'yes' OR products.on_sale = ?", @on_sale).where("products.new = 'yes' OR products.new = ?", @new).page(params[:page]).per(5)
-      end
+    elsif @artist == ''
+      @products = Product.joins(:product_type)
+                         .where('product_types.id = ?', @product_type)
+                         .where('products.on_sale = \'yes\' OR products.on_sale = ?', @on_sale)
+                         .where('products.new = \'yes\' OR products.new = ?', @new)
+                         .page(params[:page]).per(5)
+    elsif @product_type == ''
+      @products = Product.joins(:artist)
+                         .where('artists.id = ?', @artist)
+                         .where('products.on_sale = \'yes\' OR products.on_sale = ?', @on_sale)
+                         .where('products.new = \'yes\' OR products.new = ?', @new)
+                         .page(params[:page]).per(5)
+    end
     @artists = Artist.all.order(:name)
     @product_types = ProductType.all.order(:name)
     render :index
   end
 
-
+  def product_for_search
+    if params[:product][:artist_id] != ''
+      @products = Product.joins(:artist, :product_type)
+                         .where('artists.id = ? AND (artists.name ILIKE ?' \
+                         ' OR artists.name ILIKE ?' \
+                         ' OR artists.name ILIKE ?' \
+                         ' OR artists.name ILIKE ? OR products.name ILIKE ?' \
+                         ' OR products.name ILIKE ? OR products.name ILIKE ?' \
+                         ' OR products.name ILIKE ?' \
+                         ' OR product_types.name ILIKE ? OR product_types.name ILIKE ?' \
+                         ' OR product_types.name ILIKE ? OR product_types.name ILIKE ?)',
+                                params[:product][:artist_id].to_s, \
+                                "% #{params[:srch_term]}", "#{params[:srch_term]} %", \
+                                "% #{params[:srch_term]} %", params[:srch_term], \
+                                "% #{params[:srch_term]}", "#{params[:srch_term]} %", \
+                                "% #{params[:srch_term]} %", params[:srch_term], \
+                                "% #{params[:srch_term]}", "#{params[:srch_term]} %", \
+                                "% #{params[:srch_term]} %", params[:srch_term])
+                         .page(params[:page]).per(5).order(:name)
+    else
+      @products = Product.joins(:artist, :product_type)
+                         .where('artists.name ILIKE ?' \
+                                ' OR artists.name ILIKE ? OR artists.name ILIKE ? '\
+                                'OR artists.name ILIKE ? OR products.name ILIKE ? ' \
+                                ' OR products.name ILIKE ?'\
+                                ' OR products.name ILIKE ?' \
+                                ' OR products.name ILIKE ?' \
+                                ' OR product_types.name ILIKE ?'\
+                                ' OR product_types.name ILIKE ?' \
+                                ' OR product_types.name ILIKE ?' \
+                                ' OR product_types.name ILIKE ?', \
+                                "% #{params[:srch_term]}", "#{params[:srch_term]} %", \
+                                "% #{params[:srch_term]} %", params[:srch_term], \
+                                "% #{params[:srch_term]}", "#{params[:srch_term]} %", \
+                                "% #{params[:srch_term]} %", params[:srch_term], \
+                                "% #{params[:srch_term]}", "#{params[:srch_term]} %", \
+                                "% #{params[:srch_term]} %", params[:srch_term]) \
+                         .page(params[:page]).per(5).order(:name)
+    end
+  end
 end
