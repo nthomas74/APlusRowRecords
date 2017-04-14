93bcb25ca84cee8cd41ea007f0e52f1f1eb2a53f
Nicole Thomas <nicolemichellethomas@gmail.com>
2017-04-14 21:19:06 +0000

    added ability to delete cart items

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,30 @@
 class CartsController < ApplicationController
   def index
+    session_details
+  end
+
+  def refresh_cart
+    id = params[:id].to_i
+    quantity = params[:quantity].to_i
+    update_quantity(id, quantity) if params.key?(:refresh)
+    delete_item(id) if params.key?(:delete)
+  end
+
+  def update_quantity(id, quantity)
+    session[:cart].each do |item|
+      item[1] = quantity if id == item[0]
+    end
+    get_session
+  end
+
+  def delete_item(id)
+    session[:cart].each do |item|
+      session[:cart].delete(item) if id == item[0]
+    end
+    session_details
+  end
+
+  def session_details
     @cart_items = session[:cart]
     array_of_ids = []
     @total = 0
@@ -7,16 +32,6 @@ class CartsController < ApplicationController
       array_of_ids << item[0]
       @total += (Product.find(item[0]).price * item[1])
     end
-    @products = Product.where(id: array_of_ids)
-  end
-
-  def refresh_cart
-    session[:cart].each do |item|
-      item[1] = params[:quantity].to_i if params[:id].to_i == item[0]
-    end
-    @cart_items = session[:cart]
-    array_of_ids = []
-    @cart_items.each { |item| array_of_ids << item[0] }
     @products = Product.where(id: array_of_ids)
     render :index
   end
